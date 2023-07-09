b465745a7063d8a61fdcf655bf6d3ec7dc2b9b7c
gugu256 <gugu256@mail.com>
2023-07-09 23:01:43 +0200

    First actual commit (i forgor to commit the rest)

---
+++ gen.py
@@ -0,0 +1,13 @@
+import random
+import string
+import base64
+
+for key in range(1, 10001):
+  prefix = "sk-"
+  random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+  encoded_string = base64.b64encode(random_string.encode()).decode()
+
+  generated_string = prefix + encoded_string.replace("=", "")
+  print(f"Key {key} : {generated_string}")
+  with open("keys", "a") as f:
+    f.write(generated_string + "\n")
